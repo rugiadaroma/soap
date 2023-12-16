bff5a3e17466cd685fa615727c297fd53cf76959
rugiadaroma <148763601+rugiadaroma@users.noreply.github.com>
2023-12-16 21:32:43 +0100

    Delete checkdevice.js

--- checkdevice.js
+++
@@ -1,9 +0,0 @@
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-	// true for mobile device
-	console.log("mobile device");
-  }else{
-	// false for not mobile device
-	console.log("not mobile device");
-	//location.replace('https://comboni.org');
-	window.location.href = "./desktop.html";
-  }
